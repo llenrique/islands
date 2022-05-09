ffdae72015416a1460bd2e90476efb32f9b2fc5f
Enrique Lopez <lenriquelopez@gmail.com>
2022-05-08 20:16:05 -0500

    create island correctly

--- lib/islands_engine/island.ex
+++ lib/islands_engine/island.ex
@@ -5,15 +5,38 @@ defmodule IslandsEngine.Island do
 
   defstruct [:coordinates, :hit_coordinates]
 
-  def new(), do:
-    %Island{coordinates: MapSet.new(), hit_coordinates: MapSet.new()}
+  def new(type, %Coordinate{} = upper_left) do
+    with [_head_offset | _tail_offset] = offsets <- _offsets(type),
+         %MapSet{} = coordinates <- add_coordinates(offsets, upper_left) do
+      {:ok, %Island{coordinates: coordinates, hit_coordinates: MapSet.new()}}
+    else
+      error -> error
+    end
+  end
 
+  def add_coordinates(offsets, upper_left) do
+    Enum.reduce_while(offsets, MapSet.new(), fn offset, acc ->
+      _add_coordinate(acc, upper_left, offset)
+    end)
+  end
 
+  defp _add_coordinate(
+    coordinates = _island_coordinates,
+    %Coordinate{row: row, col: col} = _uper_left,
+    {row_offset, col_offset} = _offset
+  ) do
+    case Coordinate.new(row + row_offset, col + col_offset) do
+      {:ok, coordinate} -> {:cont, MapSet.put(coordinates, coordinate)}
+      {:error, :invalid_coordinate} -> {:halt, {:error, :invalid_coordinate}}
+    end
+  end
 
-  defp _offsets(:square), do: [{0,0}, {0,1}, {1,0}, {1,1}]
-  defp _offsets(:atoll), do: [{0,0}, {0,1}, {1,1}, {2,0}, {2,1}]
-  defp _offsets(:dot), do: [{0,0}]
-  defp _offsets(:l_shape), do: [{0,0}, {1, 0}, {2,0}, {2,1}]
-  defp _offsets(:s_shape), do: [{0,1}, {0,2}, {1,0}, {1,1}]
+  # Return the offset corridinates of the island shape.
+  @spec _offsets(atom()) :: [tuple()]
+  defp _offsets(:square), do: [{0, 0}, {0, 1}, {1, 0}, {1, 1}]
+  defp _offsets(:atoll), do: [{0, 0}, {0, 1}, {1, 1}, {2, 0}, {2, 1}]
+  defp _offsets(:dot), do: [{0, 0}]
+  defp _offsets(:l_shape), do: [{0, 0}, {1, 0}, {2, 0}, {2, 1}]
+  defp _offsets(:s_shape), do: [{0, 1}, {0, 2}, {1, 0}, {1, 1}]
   defp _offsets(_shape), do: {:error, :invalid_island}
 end
