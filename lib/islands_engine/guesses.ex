ff801ed778ac053915c36127de5cef22b370efe4
Enrique Lopez <lenriquelopez@gmail.com>
2022-05-08 23:00:46 -0500

    Update hitted or guesses coordintates

--- lib/islands_engine/guesses.ex
+++ lib/islands_engine/guesses.ex
@@ -1,9 +1,19 @@
 defmodule IslandsEngine.Guesses do
   alias __MODULE__
+  alias IslandsEngine.Coordinate
 
   @enforce_keys [:hits, :misses]
 
   defstruct [:hits, :misses]
 
   def new(), do: %Guesses{hits: MapSet.new(), misses: MapSet.new()}
+
+  def add(%Guesses{} = guessses, :hit, %Coordinate{} = coordinate) do
+    update_in(guessses.hits, &MapSet.put(&1, coordinate))
+  end
+
+  def add(%Guesses{} = guessses, :miss, %Coordinate{} = coordinate) do
+    update_in(guessses.misses, &MapSet.put(&1, coordinate))
+  end
+
 end
