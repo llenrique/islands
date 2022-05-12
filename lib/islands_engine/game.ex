bde4e86882442f39245aeec779d50c84da8f757b
Enrique Lopez <lenriquelopez@gmail.com>
2022-05-12 15:43:06 -0500

    new public functions, GenServer module calls and callbacks

--- lib/islands_engine/game.ex
+++ lib/islands_engine/game.ex
@@ -1,7 +1,9 @@
 defmodule IslandsEngine.Game do
   use GenServer
 
-  alias IslandsEngine.{Board, Guesses, Rules}
+  alias IslandsEngine.{Board, Guesses, Rules, Island, Coordinate}
+
+  @players [:player_one, :player_two]
 
   def init(name) do
     player_one = %{name: name, board: Board.new(), guesses: Guesses.new()}
@@ -31,6 +33,25 @@ defmodule IslandsEngine.Game do
     end
   end
 
+  def handle_call({:position_island, player, key, row, col}, _from, state) do
+    board = player_board(state, player)
+
+    with {:ok, rules} <- Rules.check(state.rules, {:position_island, player}),
+     {:ok, island_coordinate} <- Coordinate.new(row, col),
+     {:ok, island} <- Island.new(key, island_coordinate),
+     %{} = board <- Board.position_island(board, key, island) do
+      state
+      |> _update_board(player, board)
+      |> _update_rules(rules)
+      |> _reply_success(:ok)
+    else
+      :error -> {:reply, :error, state}
+      {:error, :invalid_coordinate} -> {:reply, {:error, :invalid_coordinate}, state}
+      {:error, :invalid_island_type} -> {:reply, {:error, :invalid_island_type}, state}
+      {:error, :overlapping_island} -> {:reply, {:error, :overlapping_island}, state}
+    end
+  end
+
   def add_player(game, name) when is_binary(name) do
     GenServer.call(game, {:add_player, name})
   end
@@ -42,4 +63,15 @@ defmodule IslandsEngine.Game do
     do: %{state | rules: rules}
 
   defp _reply_success(state, reply), do: {:reply, reply, state}
+
+  def position_island(game, player, key, row, col) when player in @players,
+    do: GenServer.call(game, {:position_island, player, key, row, col})
+
+  defp _update_board(state, player, board) do
+    Map.update!(state, player, fn player ->
+      %{player | board: board}
+    end)
+  end
+
+  def player_board(state, player), do: Map.get(state, player).board
 end
