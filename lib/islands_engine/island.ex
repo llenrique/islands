86cf9b83e01db54a9806ea2066c71756144081b5
Enrique Lopez <lenriquelopez@gmail.com>
2022-05-12 15:43:19 -0500

    fix error tuple

--- lib/islands_engine/island.ex
+++ lib/islands_engine/island.ex
@@ -85,5 +85,5 @@ defmodule IslandsEngine.Island do
   defp _offsets(:dot), do: [{0, 0}]
   defp _offsets(:l_shape), do: [{0, 0}, {1, 0}, {2, 0}, {2, 1}]
   defp _offsets(:s_shape), do: [{0, 1}, {0, 2}, {1, 0}, {1, 1}]
-  defp _offsets(_shape), do: {:error, :invalid_island}
+  defp _offsets(_shape), do: {:error, :invalid_island_type}
 end
